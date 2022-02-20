0728ee1ea7d1a85d8ed48fe92d0fe36e6ad8c9f7
Lázaro Vanderson <eulazzo@protonmail.com>
2022-02-20 12:44:43 -0300

    about sigma text

--- pages/sigma/[slug].js
+++ pages/sigma/[slug].js
@@ -18,17 +18,15 @@ const About = () => {
           </cite>{" "}
           and I totally agree with that. That's a good reason for making a blog,
           right? :) <br />
-          <br />
           Here I share things I've learned about{" "}
           <span className="text-purple font-semibold"> Web</span> and{" "}
-          <span className="text-purple font-semibold">
-            {" "}
-            Mobile Development{" "}
-          </span>
-          . The articles are being written in English because I want to improve
-          my English writing skills and I think that's is a very good way to do
-          it. Sorry in advance in case you found some typos, that can happen
-          very often.
+          <span className="text-purple font-semibold"> Mobile Development</span>{" "}
+          from courses, bootcamps or articles that I occasionally read to learn
+          something.
+          <br /> <br /> The articles are being written in English because I want
+          to improve my English writing skills and I think that's is a very good
+          way to do it. Sorry in advance in case you found some typos, that can
+          happen very often.
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
